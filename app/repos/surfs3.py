8736a3cb1f2e68abdb42b79a82d7ec2bc2e3ae3d
Dave Tromp <dave@davetromp.com>
2025-08-20 15:41:49 +0200

    implementation of s3 connector

--- app/repos/surfs3.py
+++ app/repos/surfs3.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from pathlib import Path
 import boto
 import boto.s3.connection
 
@@ -8,22 +10,24 @@ logger = logging.getLogger()
 class Surfs3(object):
     
     def __init__(self, api_key, user, api_address=None, *args, **kwargs):
-        self.api_address = api_address
-        if api_address is None:
-            self.api_address = os.getenv(
-                "SURFS3_API_ADDRESS", "objectstore.surf.nl"
-            )
+        try:
+            self.api_address = api_address
+            if api_address is None:
+                self.api_address = os.getenv(
+                    "SURFS3_API_ADDRESS", "objectstore.surf.nl"
+                )
 
-        self.api_key = api_key
-        self.user = user
+            self.api_key = api_key
+            self.user = user
 
-        self.s3connection = boto.connect_s3(
-            aws_access_key_id=user,
-            aws_secret_access_key=api_key,
-            host = 'objectstore.surf.nl',
-            calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-            )
-
+            self.s3connection = boto.connect_s3(
+                aws_access_key_id=user,
+                aws_secret_access_key=api_key,
+                host = 'objectstore.surf.nl',
+                calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+                )
+        except Exception as e:
+            logger.error(f"Exception at init: {e}")
 
     def check_token(self):
         """Check the API-Token `api_key`.
@@ -33,7 +37,7 @@ class Surfs3(object):
             s3buckets = self.s3connection.get_all_buckets()
             return True
         except Exception as e:
-            logger.error(e)
+            logger.error(f"Exception at check_token: {e}")
             return False
 
 
@@ -54,14 +58,15 @@ class Surfs3(object):
                 logger.error('Bucket name already exists')
                 return {'message' : 'Bucket name already exists'}
         except Exception as e:
-            logger.error(e)
+            logger.error(f"Exception at create_new_bucket: {e}")
             return {'message': f'Failed to create new bucket: {e}'}
 
 
     def upload_new_file_to_bucket(self, bucket_name, path_to_file):
         try:
             bucket = self.s3connection.get_bucket(bucket_name)
-            key_name = path_to_file.split("/")[-1]
+            key_name = path_to_file.split("./ARCHIVE/files/")[-1]
+            key_name = key_name.split("./ARCHIVE/")[-1]
             key = bucket.new_key(key_name)
             key.set_contents_from_filename(path_to_file)
             if key_name in [item.name for item in bucket.list()]:
@@ -69,5 +74,45 @@ class Surfs3(object):
             else:
                 return False
         except Exception as e:
-            logger.error(e)
+            logger.error(f"Exception at upload_new_file_to_bucket: {e}")
+            return False
+
+
+    def get_bucket_content(self, bucket_name):
+        try:
+            repo_content = []
+            bucket_name = bucket_name.split("https://")[-1]
+            bucket = self.s3connection.get_bucket(bucket_name=bucket_name)
+            for obj in bucket.list():
+                filename = obj.key
+                obj.base64md5
+                tmp = {}
+                tmp['name'] = obj.key
+                tmp['size'] = obj.size
+                tmp['link'] = ""
+                tmp['hash'] = obj.md5
+                tmp['hash_type'] = 'md5'
+                repo_content.append(tmp)
+            return repo_content
+        except Exception as e:
+            logger.error(f"Exception at download_files: {e}")
+
+    def download_files(self, bucket_name, dest_folder):
+        try:
+            bucket_name = bucket_name.split("https://")[-1]
+            bucket = self.s3connection.get_bucket(bucket_name=bucket_name)
+            for obj in bucket.list():
+                filename = obj.key.split("./")[-1]
+                localfilepathname = os.path.join(dest_folder, filename)
+                onlyfilename = localfilepathname.split("/")[-1]
+                localdirpath = Path(localfilepathname.split(onlyfilename)[0])
+                if not localdirpath.exists():
+                    r = localdirpath.mkdir(parents=True)
+                if not os.path.exists(localfilepathname):
+                    with open(localfilepathname, 'w'):
+                        pass
+                obj.get_contents_to_filename(localfilepathname)
+            return True
+        except Exception as e:
+            logger.error(f"Exception at download_files: {e}")
             return False
